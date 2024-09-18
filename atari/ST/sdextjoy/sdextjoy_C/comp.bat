e764cb9baa81a3f67d54e6ba373d5bf91ee152af
Cyril Vincent <contact@cyrilvincent.com>
2024-09-18 13:28:25 +0200

    sdextjoy_c

--- atari/ST/sdextjoy/sdextjoy_C/comp.bat
+++ atari/ST/sdextjoy/sdextjoy_C/comp.bat
@@ -1,4 +1,4 @@
-set VBCC=D:\Programmation\Falcon\VBCC
+set VBCC=C:\Users\conta\git-CVC\Retro\git-retro\atari\ST\sdextjoy\sdextjoy_C\vbcc_bin_win64
 set path=%VBCC%\bin
 
 vc +%VBCC%\config\tos -O2 -nostdlib -o sdextjoy.prg init.s main.c sd.c xbioshook.s spi_extjoy.s
